69ca0cd2c85e43b7858d0c44a8dfd5b03b610ff9
ROMEXSOFT\dkostiuk <dkostiuk@romexsoft.com>
2017-08-25 12:51:51 +0300

    Add script that builds the client and copies static files to server public directory.

--- purple-raven-server/copyStaticAssets.js
+++ purple-raven-server/copyStaticAssets.js
@@ -1,5 +1,5 @@
 var shell = require('shelljs');
 
-shell.cp('-R', 'src/public', 'dist/public');
-shell.cp('-R', 'src/public/static/css', 'dist/public/static');
-shell.cp('-R', 'src/public/static/js', 'dist/public/static');
+shell.cp('-R', '../purple-raven-client/build', 'dist/public');
+shell.cp('-R', '../purple-raven-client/build/static/css', 'dist/public/static');
+shell.cp('-R', '../purple-raven-client/build/static/js', 'dist/public/static');
